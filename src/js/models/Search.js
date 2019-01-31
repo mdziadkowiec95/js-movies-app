d635c15aa71aaf0011d5f6cdc791e0fd8e25984f
michal.dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-01-31 22:02:27 +0100

    update

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,23 +1,15 @@
 import axios from 'axios';
 import { APIkey } from '../config';
 
-// const test = async () => {
-//   const res = await axios(
-//     `http://www.omdbapi.com/?apikey=${APIkey}&s=american&y=1999`
-//   );
-//   console.log(res);
-// };
-
 export default class Search {
   constructor(query) {
     this.query = query;
   }
 
   async getResults(page) {
-    const request =
-      this.query.year > 1900
-        ? `s=${this.query.title}&y=${this.query.year}`
-        : `s=${this.query.title}`;
+    const request = this.query.year
+      ? `s=${this.query.title}&y=${this.query.year}`
+      : `s=${this.query.title}`;
 
     const results = await axios(
       `http://www.omdbapi.com/?apikey=${APIkey}&${request}&page=${page}`
